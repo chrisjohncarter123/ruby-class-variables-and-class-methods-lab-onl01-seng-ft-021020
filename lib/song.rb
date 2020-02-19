6e0a8b9a98f0a2d45b748de818511134fd2da0ed
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-19 21:17:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,15 @@
 class Song
 
-
+  attr_accessor :name, :artist, :genre
 
   @@count = 0
   @@artists = []
   @@genres = []
   def initialize(name, artist, genre)
     @@count += 1
-    
+    :name = name
+    :artist = artist
+    :genre = genre
     @@artists << artist
     @@genres << genre
     
