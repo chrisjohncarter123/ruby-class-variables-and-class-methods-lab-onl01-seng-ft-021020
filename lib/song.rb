4c9b38bc37cff5a1dee74d4e63ef4ab477ff444d
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-19 21:06:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,6 @@ end
 
 s1 = Song.new("s1", "bob", "chill")
 s2 = Song.new("s1", "bob2", "chill2")
-puts Song.count
-puts Song.artists
-puts Song.genres
+puts "count " + Song.count
+puts "artists " + Song.artists
+puts "genres " + Song.genres
