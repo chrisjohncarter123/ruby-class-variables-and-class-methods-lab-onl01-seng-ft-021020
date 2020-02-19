d049467c6986e4a848089ec4f1ead8c67bd36a9a
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-19 21:16:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
 
+
+
   @@count = 0
   @@artists = []
   @@genres = []
   def initialize(name, artist, genre)
     @@count += 1
-
+    
     @@artists << artist
     @@genres << genre
     
